e88c58d59235ffc5f9ce8d45898e137b2ced64ae
louis2704 <113580709+louis2704@users.noreply.github.com>
2023-11-06 15:01:22 +0100

    Update Deployment.sol

--- script/Deployment.sol
+++ script/Deployment.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import {Script} from "forge-std/Script.sol";
 import {Evaluator} from "../src/EvaluatorToken.sol";
 import {RewardToken} from "../src/RewardToken.sol";
+import {TokenERC20} from "../src/ERC20.sol";
 
 contract DeploymentScript is Script {
     function run() external {
@@ -12,6 +13,8 @@ contract DeploymentScript is Script {
 
         RewardToken rewardToken = new RewardToken();
         new Evaluator(rewardToken);
+
+        TokenERC20 myErc20 = new TokenERC20();
 
         vm.stopBroadcast();
     }
